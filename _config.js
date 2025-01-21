35c4b38f43c7144a9f2639f9a7112b5ca01db7c4
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-01-21 19:04:11 +0300

    update db credentials

--- _config.js
+++ _config.js
@@ -1,9 +1,11 @@
-var config = {}
+var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+	production:
+		'mongodb+srv://nelsonmuriithi:rFaA6yMQ9A9QXFDF@cluster0.8sc8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+	development:
+		'mongodb+srv://nelsonmuriithi:rFaA6yMQ9A9QXFDF@cluster0.8sc8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+	test: 'mongodb+srv://nelsonmuriithi:rFaA6yMQ9A9QXFDF@cluster0.8sc8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+};
 module.exports = config;
